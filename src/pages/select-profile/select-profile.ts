e0527d8644d5d20ebdcaf2187c272b1e5c40a8c5
andrewgarver <andrewsgarver@gmail.com>
2017-01-26 04:36:10 -0700

    Next up, job and customer details.

--- src/pages/select-profile/select-profile.ts
+++ src/pages/select-profile/select-profile.ts
@@ -15,6 +15,9 @@ export class SelectProfilePage {
   constructor(public navCtrl: NavController, private params: NavParams) {
     this.user = JSON.parse(localStorage.getItem('current_user'));
     this.switchProfile = params.get('switch_profile');
+
+    // This is just in case the user exits the app before selecting a profile, we don't want them in limbo.
+    localStorage.setItem('current_profile', JSON.stringify(this.user.profiles[0]));
   }
 
   setProProfile() {
