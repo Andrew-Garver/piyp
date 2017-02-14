3c355931b960066b45fd42896f6688438a632cc4
Justin <jdeanwaite@gmail.com>
2017-02-14 12:30:20 -0700

    Added functionality for Q&A on jobs, as well as new logo and background image.

--- src/question-details/question-details.ts
+++ src/question-details/question-details.ts
@@ -6,21 +6,25 @@ import {FormGroup, Validators, FormBuilder} from "@angular/forms";
 import {LoadingService} from "../services/loading.service";
 import {ToastService} from "../services/toast.service";
 import {LoginPage} from "../pages/login/login";
+import {JobService} from "../services/job.service";
 
 @Component({
   selector: 'page-question-details',
   templateUrl: 'question-details.html',
-  providers: [AuthService, LoadingService, ToastService]
+  providers: [AuthService, LoadingService, ToastService, JobService]
 })
 
 export class QuestionDetailsPage {
   private formAnswerQuestion: FormGroup;
   private selectedQuestion: any;
   private profileType: any;
+  private job: any;
+  private question: any;
 
   constructor(private navCtrl: NavController, private params: NavParams,
               private authService: AuthService, private formBuilder: FormBuilder,
-              private loadingService: LoadingService, private toastService: ToastService) {
+              private loadingService: LoadingService, private toastService: ToastService,
+              private jobService: JobService) {
     this.selectedQuestion = params.get("question");
     this.profileType = JSON.parse(localStorage.getItem('current_profile')).type;
 
@@ -29,44 +33,52 @@ export class QuestionDetailsPage {
     });
   }
 
+  ionViewWillEnter() {
+    this.job = this.params.get('job');
+    this.question = this.params.get('question');
+  }
+
   ionViewCanEnter(): Promise<boolean> {
     return new Promise((resolve, reject) => {
       this.authService.loggedIn()
-        .then((data) => {
-          if (data) {
-            resolve(true);
-          }
-          else {
-            resolve(false);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          reject(err);
-        });
+          .then((data) => {
+            if (data) {
+              resolve(true);
+            }
+            else {
+              resolve(false);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            reject(err);
+          });
     });
   }
 
   answerQuestion() {
     if (this.formAnswerQuestion.valid) {
       this.loadingService.presentLoading();
-      this.postData()
-        .then(() => {
-          this.loadingService.hideLoading();
-          this.navCtrl.pop()
-            .catch(() => {
-              this.authService.logout()
-                .then(() => {
-                  this.navCtrl.setRoot(LoginPage);
-                  this.toastService.presentToast("Your session has expired. Please login again.");
+      this.jobService.answerQuestion(this.job._id, this.question._id, this.formAnswerQuestion.value.answer)
+          .then((result) => {
+            if (result) {
+              this.job.qa = result;
+            }
+            this.loadingService.hideLoading();
+            this.navCtrl.pop()
+                .catch(() => {
+                  this.authService.logout()
+                      .then(() => {
+                        this.navCtrl.setRoot(LoginPage);
+                        this.toastService.presentToast("Your session has expired. Please login again.");
+                      });
                 });
-            });
-        })
-        .catch((err) => {
-          console.log(err);
-          this.loadingService.hideLoading();
-          this.toastService.presentToast("Could not reach PIYP servers. Check your data connection and try again.")
-        });
+          })
+          .catch((err) => {
+            console.log(err);
+            this.loadingService.hideLoading();
+            this.toastService.presentToast("Could not reach PIYP servers. Check your data connection and try again.")
+          });
     }
   }
 
